2c253fb740bcad5d68c0438f55055d257b303614
mojomastur <39616381+mojomastur@users.noreply.github.com>
2018-10-20 23:06:23 +0200

    launch script for polybar

---
+++ 12 Polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pkill polybar
+polybar top &
+#polybar top2 &
+polybar A1 &
+polybar A2 &
+polybar B1 &
+polybar B2 &
+polybar C1 &
+polybar C2 &
+polybar D1 &
+polybar D2 &
+polybar E1 &
+polybar E2 &
+polybar bottom &
+
+exit 0
